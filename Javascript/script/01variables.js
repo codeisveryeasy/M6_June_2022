38ab9b2321c4b210182bb23acd1f86d6b62b3e83
codeisveryeasy <89445533+codeisveryeasy@users.noreply.github.com>
2022-07-15 19:32:55 +0530

    day 02 for JS

---
+++ Javascript/script/01variables.js
@@ -0,0 +1,47 @@
+console.log("Hello Variables!")
+console.log("It is being logged to the console!")
+
+var name = "Air Asia Academy"
+
+console.log(name)
+
+var score1 = 88
+var score2 = 22
+
+var scoreTotal = score1 + score2
+console.log(scoreTotal)
+
+var message  = "I am student of software engineering at " + name
+
+console.log(message)
+
+var percent  = 88.88
+console.log(percent)
+
+var check = true
+console.log(check)
+
+
+console.log(typeof(score1))
+//'number'
+console.log(typeof(name))
+//'string'
+console.log(typeof(percent))
+//'number'
+console.log(typeof(check))
+//'boolean'
+
+console.log("Value contained in variable score1 is of type " + typeof(score1))
+//'number'
+console.log("Value contained in variable name is of type " + typeof(name))
+//'string'
+console.log("Value contained in variable percent is of type " + typeof(percent))
+//'number'
+console.log("Value contained in variable check is of type " + typeof(check))
+
+
+var age = 31
+
+let myage = 41
+
+const yourage = 21
---
+++ Javascript/script/02arthmetic.js
@@ -0,0 +1,11 @@
+console.log("Arithmetic Operations")
+
+var score  = 88
+
+console.log(score  + 1)
+console.log(score)
+console.log(score++)
+console.log(score)
+console.log(score*2)
+console.log(score/4)
+console.log(score % 2)
