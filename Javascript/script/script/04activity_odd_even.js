1d3730b91581fed8cc5bffeafbf762cae9f85cc9
codeisveryeasy <89445533+codeisveryeasy@users.noreply.github.com>
2022-07-19 19:35:14 +0530

    Add files via upload

---
+++ Javascript/script/script/04activity_odd_even.js
@@ -0,0 +1,22 @@
+console.log("Odd even number check")
+
+var inputNumber = prompt("Enter any number: ")
+console.log(inputNumber)
+
+//write if condition with correct arithmetic operator
+//to check if number is even or odd.
+
+// if number is even
+// output in console should Be 
+//"Number n is even"
+
+// if number is odd
+// output in console should Be 
+//"Number n is odd"
+
+if(inputNumber % 2 === 0){
+    console.log("Number " + inputNumber + " is even")
+}else{
+    console.log("Number " + inputNumber + " is odd")
+}
+
